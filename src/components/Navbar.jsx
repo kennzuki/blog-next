704403d0d8e7d29271745bacbc1c600cce681d29
kennzuki <kingsyken@gmail.com>
2024-11-15 15:21:07 +0300

    Toggle button for mobile/small screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
+  
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
+
   const navItems = [
     { path: '/', link: 'Home' },
     { path: '/about', link: 'About' },
@@ -11,13 +22,13 @@ const Navbar = () => {
   ];
  
   return (
-    <header className='bg-black'>
-      <nav className='p-4 flex justify-between gap-6 place-items-center'>
+    <header className='bg-black text-white'>
+      <nav className='p-4 flex justify-between gap-6 place-items-center max-w-7xl mx-auto'>
         <a href='' className='text-3xl text-white font-bold uppercase'>
           Ken<span className="text-yellow-400">ki</span> 
         </a>
         {/* nav links */}
-        <ul className='md:flex place-items-center gap-12 text-lg'>
+        <ul className='md:flex place-items-center gap-12 text-lg hidden'>
                   {navItems.map(({ path, link }) => (
                       <li className="" key={path}>
                           <NavLink  to={path} className='text-white hover:text-gray-400'>
@@ -36,7 +47,13 @@ const Navbar = () => {
               Login
             </button>
           </li>
-        </ul>
+              </ul>
+              {/* small screen */}
+              <div className="">
+          <button onClick={toggleMenu} className="cursor-pointer md:hidden">
+            {isOpen ? <FaXmark className="w-5 h-5"/> : <FaBars className="w-5 h-5"/> }
+           </button>
+              </div>
       </nav>
     </header>
   );
