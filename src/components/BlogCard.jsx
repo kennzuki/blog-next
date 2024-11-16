93302507a26e890a6e926b6261f01a9e5c63a56f
kennzuki <kingsyken@gmail.com>
2024-11-16 19:21:52 +0300

    fetching and mapping of the blog data in blogPage

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+
+const BlogCard = ({ blogs }) => {
+    const filteredBlogs = blogs
+  return (
+     
+    
+  )
+}
+
+export default BlogCard
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,10 +1,20 @@
+import BlogPage from "../components/BlogPage";
+
 const Blog = () => {
   return (
-    <div className='py-40 bg-black text-center text-white px-4'>
-      <div className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>
+    <div className="">
+      <div className='py-40 bg-black text-center text-white px-4'>
+      <h2 className=' text-5xl lg:text-7xl leading-snug font-bold mb-5'>
         Blogs
+      </h2>
+    
+      </div>
+        {/* all blogs */}
+        <div className="max-w-7 p-4">
+        <BlogPage/>
       </div>
     </div>
+    
   );
 };
 
